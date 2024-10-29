df864b204425f1a367fe063df23423f510298460
eugide <eugidendungutse770@gmail.com>
2024-10-29 15:50:52 +0200

    Exercis of React

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return ( <div>
+    <h1 className="text-2xl text-center mb-6">Blog</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur atque natus ab ratione asperiores omnis unde facilis, accusamus ad numquam non culpa 
+      
+      est a consectetur architecto odit eligendi? Eum voluptatibus, earum reiciendis enim est, iure tempore adipisci ea praesentium doloremque, accusantium vero tenetur eligendi 
+      culpa deleniti possimus aut suscipit in corporis soluta accusamus sed voluptatem! Maiores quas neque voluptates aliquid id veniam! Odit, velit iure labore 
+      
+      aliquid aperiam ipsa. Praesentium vero, voluptate odit doloribus molestiae autem. Sit quos temporibus voluptatum iste voluptas, minus earum unde. Eos, aut? Quia vel doloremque 
+      vero nesciunt consequatur fugit at distinctio eligendi 
+      temporibus illo maxime repellendus iste inventore, tempore architecto voluptates iure accusamus quos error neque dolorum praesentium repudiandae id nam. 
+      
+      Cum excepturi itaque voluptatum totam, voluptates tempora reprehenderit ea non natus esse cupiditate ab neque at nobis praesentium quam minus pariatur deleniti tenetur saepe. 
+      Voluptate nisi quidem, culpa tempora asperiores veniam harum eaque aut atque architecto in odio excepturi rerum? Incidunt quam accusamus, eveniet, tempore 
+      
+      necessitatibus assumenda repellendus labore totam non, pariatur eius. Veniam autem numquam cum earum adipisci, quasi repellat, tenetur quam repellendus molestias nostrum 
+      possimus laboriosam deserunt voluptatem doloremque! Officia eveniet quae nihil eligendi modi culpa dicta? Inventore optio quis laboriosam aperiam.</p>
+  </div> );
+}
+ 
+export default App;
